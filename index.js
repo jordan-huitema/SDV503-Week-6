be41825543763ebb61dd84e087e61889ceab3110
Jordan Huitema <Jordan-Huitema@live.nmit.ac.nz>
2020-04-06 12:30:19 +1200

    added index.js and started function lesson

---
+++ index.js
@@ -0,0 +1,21 @@
+//-----Functions Pt.1-----
+
+//Paramater = Used slots in conditions
+
+//Argument = The value of a paramater
+
+//executing a function
+function add(v1,v2) {
+    return v1 + v2;
+}
+
+console.log(add(1,1))
+
+
+//hardcoding paramaters
+function eg(v1=10) {
+    console.log(v1)
+}
+
+eg()
+
